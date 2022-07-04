2aa5e713f4c6843bc78676cda16de1921744f17d
aadrian00 <aaai2007@hotmail.com>
2022-07-03 23:05:44 -0500

    Tablas completas

--- Base.sql
+++ Base.sql
@@ -63,7 +63,60 @@ CREATE Table MedioDigital (
     NroVisitas number (20)
 );
 
+CREATE Table Comprador (
+    Codigo number (3),
+    Dni number (8),
+    Telefono number (9),
+    Presupuesto number (9),
+    CorreoElec varchar2 (50),
+    FechaNac date,
+    PrimerNombre varchar2 (20),
+    SegundoNombre varchar2 (20),
+    ApellidoPaterno varchar2 (20),
+    ApellidoMaterno varchar2 (20),
+    CodMedio number (3)
+);
 
+CREATE Table Inmueble (
+    Codigo number (3),
+    Material varchar2 (15),
+    CRI varchar2 (2),
+    AreaTotal number (4),
+    NroBaños number (2),
+    NroHabitac number (2),
+    Distrito varchar2 (20),
+    Provincia varchar2 (15),
+    Calle_Jiron varchar2 (30),
+    ColorExterior varchar2 (20),
+    Ocupado varchar2 (2),
+    AreaTechada number (3),
+    Antiguedad number (2),
+    CodOfert number (3)
+);
 
+CREATE Table Estacionamiento (
+    CodInmb number (3),
+    Area number (4),
+    Capacidad number (3)
+);
 
+CREATE Table Apartamento (
+    CodInmb number (3),
+    Piso number (2), 
+    Parilla varchar2 (2),
+    Elevador varchar2 (2)  
+);
+
+CREATE Table LocalComercial (
+    CodInmb number (3),
+    Aforo number (4)
     
+);
+
+CREATE Table Casa (
+    CodInmb number (3),
+    Pisos number (1),
+    Jardin varchar2 (2),
+    Terraza varchar2 (2)
+    
+);
